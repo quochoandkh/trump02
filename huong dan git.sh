04c20f14146639fd64105d102bc69b63637b310e
quochoandkh <quochoandkh@gmail.com>
2024-05-17 16:20:35 +0700

    Add a.txt

---
+++ huong dan git.sh
@@ -0,0 +1,10 @@
+cd đường/dẫn/đến/thư/mục/dự/án
+git init # khởi tạo git
+git remote add origin https://github.com/tên-người-dùng/tên-repository.git # kết nối với repository mới được tạo
+git add . # để đưa toàn bộ thư mục lên
+git commit -m "Initial commit"
+git push -u origin main # sau bước này sẽ push dự án lên git
+
+lấy dự án về: 
+git clone https://github.com/quochoandkh/trump02.git
+cd repo
